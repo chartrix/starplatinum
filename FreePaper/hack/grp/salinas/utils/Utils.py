f2574d6dba969b862699bc1d48f9a86dbaa802f9
Eder Villanueva Cortez <eder.villanueva.cortez@bbva.com>
2019-08-24 16:35:44 -0500

    Base repository

---
+++ FreePaper/hack/grp/salinas/utils/Utils.py
@@ -0,0 +1,5 @@
+import hack.grp.salinas.utils
+
+class Calcu:
+    def suma(self,a,b):
+        return a+b
---
+++ FreePaper/setup.py
@@ -0,0 +1,30 @@
+from flask import Flask, escape, request
+from PIL import Image, ImageDraw
+
+app = Flask(__name__)
+
+@app.route('/turnos')
+def turno():
+    name = request.args.get("name", "turno")
+    return f'Hello, {escape(name)}!'
+
+@app.route('/tickets/<telephone>')
+def ticket_by_telephone(telephone):
+    name = request.args.get("name", "tickets")
+
+    # FASE 1 BUSQUEDA DE TICKETS POR TELEPHONO
+
+    # FASE 2 CREACION DE TICKET EN IMAGEN SAVE LOCAL
+    img = Image.new('RGB', (100, 100), color = (73, 109, 137))
+    d = ImageDraw.Draw(img)
+    ticketValue = 'Hello Eder \n Telefono: {telephone}!'
+
+    d.text((10, 10), ticketValue, fill=(255, 255, 0))
+    img.save(telephone+'_ticket.png')
+
+
+    # FASE 3 UTILS de COMUNICACION CON WHATSAPP/TELEGRAM/ETC
+
+
+
+    return '{status:ok}'
